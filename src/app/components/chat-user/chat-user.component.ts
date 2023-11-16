1a2aaf293dbafb22d991587c3b569018203b5a35
MDrschl <117458298+MDrschl@users.noreply.github.com>
2023-11-16 19:46:09 +0100

    Story 5 Update

--- src/app/components/chat-user/chat-user.component.ts
+++ src/app/components/chat-user/chat-user.component.ts
@@ -3,7 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-chat-user',
   templateUrl: './chat-user.component.html',
-  styleUrls: ['./chat-user.component.css']
+  styleUrls: ['./chat-user.component.css'],
 })
 export class ChatUserComponent {
   @Output() userCreated = new EventEmitter<string>();
@@ -12,15 +12,24 @@ export class ChatUserComponent {
   public errorMessage = '';
   public disableInput = false;
 
+  private createdUsernames: string[] = [];
+
   public createUser(): void {
-    if (this.username.trim() === '') {
+    const trimmedUsername = this.username.trim();
+
+    if (trimmedUsername === '') {
       this.errorMessage = 'Bitte fügen Sie einen Benutzer hinzu';
-    } else if (this.username.length > 12) {
+    } else if (trimmedUsername.length > 12) {
       this.errorMessage = 'Warnung: Der Benutzername überschreitet 12 Zeichen!';
       this.disableInput = true;
+    } else if (this.createdUsernames.includes(trimmedUsername)) {
+      this.errorMessage = 'Dieser Benutzername ist bereits vorhanden.';
+    } else if (trimmedUsername.includes(' ')) {
+      this.errorMessage = 'Der Benutzername darf keine Leerzeichen enthalten.';
     } else {
       this.errorMessage = '';
-      this.userCreated.emit(this.username);
+      this.userCreated.emit(trimmedUsername + '\n'); // Hinzufügen des Zeilenumbruchs
+      this.createdUsernames.push(trimmedUsername);
       this.username = '';
       this.disableInput = false;
     }
